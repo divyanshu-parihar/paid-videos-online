8a467fe4b096438d4cc9926576c8d4b5709c9a87
Divyanshu Parihar <divyanshu1447@gmail.com>
2024-01-27 17:15:12 +0530

    Delete backend/dist directory

--- backend/dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-const path = require("path");
-(0, dotenv_1.config)();
-exports.default = {
-    googleServiceAccountEmail: process.env.GOOGLE_SERVICE_ACCOUNT_INFO,
-    KeyPath: (process.env.PRODUCTION ? path.join(__dirname, 'GCP_KEY.json') : '/Applications/Divyanshu Parihar/typescript-projects/paid-videos/backend/GCP_KEY.json')
-};
